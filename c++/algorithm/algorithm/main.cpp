4cc6abab96a229050c24a54b73ea0ff447b3143e
justin.wang <justin.wang@zerofinance.com>
2021-05-10 08:54:48 +0800

    add

---
+++ c++/algorithm/algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by justin.wang on 2021/5/8.
+//
+
+#include <iostream>
+
+int main0(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
